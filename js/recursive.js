fde3777fbf5dd4437849214390f3a2e8d1f692fc
dinesh khadka <dnshka@gmail.com>
2015-06-04 11:58:10 +0545

    Made some changes to functions

--- js/recursive.js
+++ js/recursive.js
@@ -18,11 +18,23 @@ notifDict = {'t1': 900000, 't2':1800000, 't3':3600000}
 	- Gets the time remaining in milliseconds
 	- Sets that as the timeout
 	- Once time runs out, it fires a callback and the recursion kickstarts. Which then refreshes everything
+    
+        console.log(filterTime)
+        console.log(jkFartSparkles(timetick))
+        console.log("This Instant: "+thisInstant)
+        console.log('This: '+jkFartSparkles(timetick))
+        console.log('TimeTick: '+ timetick)
+        console.log("Delorean: "+ deLorean)
+        console.log(notifDict[before])
+
 */
 
 
 
 console.log('It\'s alive')
+
+
+
 function recursive(){
    console.log('Stormborn')
     
@@ -32,26 +44,25 @@ function recursive(){
         var filterTime = jkTimeStatus(thisInstant, jkMillify());
         var timetick = filterTime[2]-thisInstant;
         var before = localStorage.jkNotifyID
-      
+
+        var timer;
+        var timerStarted = false
 
         var deLorean = filterTime[2]-parseInt(notifDict[before])
-
-
         console.log(filterTime)
-        console.log(jkFartSparkles(timetick))
-        console.log("This Instant: "+thisInstant)
-        console.log('This: '+jkFartSparkles(timetick))
-        console.log('TimeTick: '+ timetick)
-        console.log("Delorean: "+ deLorean)
-        console.log(notifDict[before])
-        //
+        if (timerStarted != false){
+            clearTimeOut(timer)
+        }
+        
+
         var msgTemplate = jkFartSparkles(notifDict[before])[1] + ' Minutes left till'
         msgTemplate += (filterTime[0] == true) ? ' power on': ' power off';
         console.log(msgTemplate)
 
         if (thisInstant > deLorean){
             // Sleep until power on/off and restart the function
-            setTimeout(recursive, timetick);
+            var timer = setTimeout(recursive, timetick);
+            timerStarted = true;
             //setTimeout(recursive, 4000);
             hideForNow = true;
             console.log("Fartsparkles timeticked:"+ jkFartSparkles(timetick))
@@ -67,7 +78,8 @@ function recursive(){
                 jkNotifyUser('Junkiree', 'img/bulb-on.png', msgTemplate);
 
               }
-            setTimeout(recursive, (deLorean - thisInstant))
+            timer = setTimeout(recursive, (deLorean - thisInstant))
+            timerStarted = true
             //setTimeout(recursive, 3000);
             console.log("Fartsparkles Delorean:"+ jkFartSparkles(deLorean - thisInstant))
             hideForNow = false;
