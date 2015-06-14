96046a6e118a511e0c635ad600638e51b7605388
dinesh khadka <dnshka@gmail.com>
2015-06-14 11:02:50 +0545

    Refactorings and simple code changes

--- js/initialize.js
+++ js/initialize.js
@@ -9,34 +9,13 @@ Copyright (C) 2015 by Dinesh Khadka [http://junkiree.github.io]
 //require('recursive.js')
 var nepMonth = ['Baisakh', 'Jestha', 'Ashar', 'Shrawan', 'Bhadra', 'Ashoj', 'Kartik', 'Mangsir', 'Poush', 'Magh', 'Falgun', 'Chaitra'];
 
-function init() {
-    /* 
-		Search for the group Index. If not found, assume this is the first install
-		and other options are not stored as well
-	 	since the group index and the schedule itself is the important part
-	*/
+$('document').ready(function(){
+    if (localStorage.jkScheduleJSON != undefined) {
 
-    // TODO: Specifying initialize.js as a background script throws a TypeError
-    //       since the script is supposed to be imported by popup.html. Fix it.
-
-    if (localStorage.getItem('jkScheduleJSON') != undefined) {
-
-
-
-
-
-        // Initialize the current Day
-
-        var jkSetToday = document.getElementById('style-today');
-        jkSetToday.innerHTML += jkFullDayArray[dataList().jkDayIndex];
-        // end
-
-
-        // Initialize Current Time
-        var ctime, whatsItsName;
+        var ctime;
+        var whatsItsName = '';
         thisHour = DateObject.getHours();
         thisMinute = DateObject.getMinutes();
-        whatsItsName = '';
         var addZero = (thisMinute < 10) ? '0' : '';
         if (isMilitary == 'false') {
             whatsItsName = ' AM';
@@ -44,45 +23,33 @@ function init() {
                 thisHour -= 12;
                 whatsItsName = ' PM';
             }
-
-
-
         }
-        // end
 
-
-        // Initialize schedule times for today and tommorow
-        var jkSetCurTime = document.getElementById('style-current');
-        jkSetCurTime.innerHTML += thisHour + ':' + addZero + thisMinute + whatsItsName;
-
-        var jkSetGroup = document.getElementById('style-group');
-        jkSetGroup.innerHTML += jkGetSchedule(dataList().jkDefGroup(), 'title');
+        $('#style-today').html(jkFullDayArray[dataList().jkDayIndex])
+        $('#style-current').html(thisHour + ':' + addZero + thisMinute + whatsItsName);
+        $('#style-group').html(jkGetSchedule(dataList().jkDefGroup(), 'title'));
 
 
         jkAppendTimes('style-tommorow', dataList().jkDefGroup(), dataList().jkNextDayPrefix());
         jkAppendTimes('style-times', dataList().jkDefGroup(), dataList().jkDayPrefix);
 
         if (isMilitary == 'false') {
-            document.getElementById('style-times').classList.add('meridian-today');
-            document.getElementById('style-tommorow').classList.add('meridian-tommorow');
+            $('#style-times').addClass('meridian-today')
+            $('#style-tommorow').addClass('meridian-tommorow')
         } else {
-            document.getElementById('style-times').classList.add('military-today');
-            document.getElementById('style-tommorow').classList.add('military-tommorow');
+            $('#style-times').addClass('military-today');
+            $('#style-tommorow').addClass('military-tommorow');
         }
-        // end
 
-        //Initialize Nepali Date
-        // Thanks to @prashishh for the date converter library
+
         var main = new Object();
+        var output;
+
         main = engtonep.DateConversion(DateObject.getDate(), DateObject.getMonth() + 1, DateObject.getFullYear());
 
         d = main.getDate();
         m = main.getMonth();
         y = main.getYear();
-
-
-        var jkSetDate = document.getElementById('style-date');
-        var output;
 
         if (d >= 11 && d <= 13) {
             output = 'th';
@@ -93,18 +60,8 @@ function init() {
 
         }
 
-        jkSetDate.innerHTML += d + '<sup>' + output + '</sup> of ' + nepMonth[m - 1]; //+', '+y+' B.S';
-        //end
-
-
-
-        // Initialize Remaining time in HH:MM format
-        var jkSetRemaining = document.getElementById('style-remaining');
-        jkSetRemaining.innerHTML += remaining();
-
-
-
-        // Initialize a circular progressbar using jQuery knob plugin
+        $('#style-date').html(d + '<sup>' + output + '</sup> of ' + nepMonth[m - 1]); //+', '+y+' B.S';
+        $('#style-remaining').html(remaining())
         $('.knob').knob();
 
         progress(powerStatus[1], powerStatus[2], curMS);
@@ -126,19 +83,6 @@ function init() {
             });
         }
 
-
-
-
-        // Check update Once a day
-
-        if (localStorage.getItem('jkAutoUpdate') == 'true' && localStorage.getItem('lastUpdate') != DateObject.getDay()) {
-
-            jkParseRemoteSchedule(jkScheduleURL);
-
-        }
-        localStorage.setItem('lastUpdate', DateObject.getDay());
-        console.log('Today is: ' + DateObject.getDay());
-
     } else {
         chrome.tabs.create({
             'url': 'first-run.html',
@@ -147,6 +91,9 @@ function init() {
 
 
     }
-}
 
-window.onload = init;
+
+});
+
+
+
